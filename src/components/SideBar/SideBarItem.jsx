240a94cdc574590c8870f3e7b6433b18e7856497
wanda <rashiidahweira@gmail.com>
2023-11-08 20:37:42 +0300

    Alignment issue on chevron fixed

--- src/components/SideBar/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem.jsx
@@ -3,20 +3,26 @@ import { Link } from "react-router-dom";
 import { HiChevronDown, HiChevronUp } from "react-icons/hi";
 
 const SideBarItem = ({ dest, text, icon, chevron, children, chevronClick }) => {
+  const chevronStyle = {
+    marginLeft: 'auto', // Aligns the chevron to the right
+  };
+
   return (
     <Link to={dest}>
       <div className="group flex items-center gap-4 py-4 px-6  transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
         {icon}
         <p className="text-white text-sm font-poppins">{text}</p>
-        {!chevron ? (
-          <HiChevronDown
-            onClick={chevronClick}
-            className=" ms-[70px] text-white text-xl"
-          />
-        ) : (
+        {chevron ? (
           <HiChevronUp
             onClick={chevronClick}
-            className=" ms-[70px] text-white text-xl"
+            className="text-white text-xl"
+            style={chevronStyle}
+          />
+        ) : (
+          <HiChevronDown
+            onClick={chevronClick}
+            className="text-white text-xl"
+            style={chevronStyle}
           />
         )}
       </div>
