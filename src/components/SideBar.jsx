b4bb7907e2b9a9f9999aaf472ed0708ed95b5b91
wanda <rashiidahweira@gmail.com>
2023-11-08 15:43:22 +0300

    Rest of sidebar navigation

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -57,42 +57,69 @@ const SideBar = () => {
             <p className="text-white text-sm font-poppins">Inventory</p>
           </div>
         </Link>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <TbReportAnalytics className="text-white" />
-          <p className="text-white text-sm font-poppins">Reports</p>
-        </div>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <VscSettings className="text-white" />
-          <p className="text-white text-sm font-poppins">Configurations</p>
-        </div>
+        <Link to="/reports">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <TbReportAnalytics className="text-white" />
+            <p className="text-white text-sm font-poppins">Reports</p>
+          </div>
+        </Link>
+        <Link to="/configurations">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <VscSettings className="text-white" />
+            <p className="text-white text-sm font-poppins">Configurations</p>
+          </div>
+        </Link>
+
         <div className="h-[0px] border border-gray-600" />
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <MdOutlinePeopleOutline className="text-white" />
-          <p className="text-white text-sm font-poppins">Contact Management</p>
-        </div>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <MdNotificationsNone className="text-white" />
-          <p className="text-white text-sm font-poppins">Notifications</p>
-        </div>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <MdOutlineMessage className="text-white" />
-          <p className="text-white text-sm font-poppins">Chat with Visitors</p>
-        </div>
+        <Link to="/contactManagement">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <MdOutlinePeopleOutline className="text-white" />
+            <p className="text-white text-sm font-poppins">
+              Contact Management
+            </p>
+          </div>
+        </Link>
+
+        <Link to="/contactManagement">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <MdNotificationsNone className="text-white" />
+            <p className="text-white text-sm font-poppins">Notifications</p>
+          </div>
+        </Link>
+
+        <Link to="/chat">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <MdOutlineMessage className="text-white" />
+            <p className="text-white text-sm font-poppins">
+              Chat with Visitors
+            </p>
+          </div>
+        </Link>
+
         <div className="h-[0px] border border-gray-600" />
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <MdOutlinePeopleOutline className="text-white" />
-          <p className="text-white text-sm font-poppins">
-            Application Settings
-          </p>
-        </div>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <FaVirusCovid className="text-white" />
-          <p className="text-white text-sm font-poppins">Covid 19</p>
-        </div>
-        <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
-          <MdOutlineHelpCenter className="text-white" />
-          <p className="text-white text-sm font-poppins">Get Technical Help</p>
-        </div>
+        <Link to="/appSettings">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <MdOutlinePeopleOutline className="text-white" />
+            <p className="text-white text-sm font-poppins">
+              Application Settings
+            </p>
+          </div>
+        </Link>
+
+        <Link to="/covid19">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <FaVirusCovid className="text-white" />
+            <p className="text-white text-sm font-poppins">Covid 19</p>
+          </div>
+        </Link>
+        <Link to="/help">
+          <div className="group flex items-center gap-4 py-4 px-6 transition duration-300 ease-in-out hover:bg-teal-400 active:bg-teal-400 ">
+            <MdOutlineHelpCenter className="text-white" />
+            <p className="text-white text-sm font-poppins">
+              Get Technical Help
+            </p>
+          </div>
+        </Link>
       </div>
     </aside>
   );
