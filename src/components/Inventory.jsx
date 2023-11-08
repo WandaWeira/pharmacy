39f437b9b32468e89d25556abde757bec09af9ca
wanda <rashiidahweira@gmail.com>
2023-11-08 15:30:27 +0300

    Inventory Home page completed

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import CardSmall from "./CardSmall";
+import { MdAdd } from "react-icons/md";
 
 const Inventory = () => {
   return (
-    <div>Inventory</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <div className="">
+          <h2 className="text-xl font-bold">Inventory</h2>
+          <h3>List of medicines available for sale</h3>
+        </div>
+        <button className="flex justify-between items-center rounded p-2 border-2 text-white bg-red-500">
+          <MdAdd className="w-5" />
+          <h3>Add New Item</h3>
+        </button>
+      </div>
+      <CardSmall />
+    </>
+  );
+};
 
-export default Inventory
+export default Inventory;
